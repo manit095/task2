c73f951957cd005524eb551b481cdc2afd75102f
Manit Kumar <146991929+manit095@users.noreply.github.com>
2025-01-28 23:16:18 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.5.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+  client_id = "<Your client id>"
+  client_secret = "<Your secret key>"
+  tenant_id = "<Your tenet id>"
+  subscription_id = "<Your subcription id>"
+}
